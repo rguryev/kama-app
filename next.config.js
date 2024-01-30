2fc835a14a5c1b793dbe8c32c4b4aee0f27158f8
rguryev <rgguryev@gmail.com>
2024-01-30 15:54:05 +0100

    fix: Edit nextConfig to avoid alias issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "standalone",
+  webpack: (config, { isServer }) => {
+    config.resolve.alias["~"] = path.join(__dirname, "src");
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
